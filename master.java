2dc8625bf19c49ff161837502bac554c85f8582b
PriyaRaviit3 <34087852+PriyaRaviit3@users.noreply.github.com>
2017-12-19 12:25:06 +0530

    Add files via upload

---
+++ master.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class master extends Frame
+ {
+	master()
+	{
+		setVisible(true);
+
+		setLayout(null);
+		setSize(1000,1000);
+		setBackground(Color.red);
+	}
+}
+class demomaster
+ {
+	public static void main(String args[])
+	{
+		//master obj=new master();
+	}
+}
