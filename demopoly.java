9f21a78edceda56e25877557b757953694c67868
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-04-04 11:22:34 +0530

    Add files via upload

---
+++ demopoly.java
@@ -0,0 +1,28 @@
+	class sample
+	{
+		public void display()
+		{
+			System.out.println("this is polymorphism");
+		}
+
+		public void display(String s, String m, String p)
+		{
+			System.out.println(s+m+p);
+		}
+		
+		public void display(int x,int y)
+		{
+			System.out.println("sum="+(x+y));
+		}
+	}
+	class demopoly
+	{
+		public static void main(String args[])
+			{
+				sample obj=new sample();
+				obj.display();
+				obj.display("salem","and","yercadu");
+				obj.display(10,10);
+			}
+	}
+
