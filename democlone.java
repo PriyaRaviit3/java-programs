6d8285a636376800ba18c5954be83d85625733aa
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-04-04 11:22:07 +0530

    Add files via upload

---
+++ democlone.java
@@ -0,0 +1,27 @@
+class democlone implements Cloneable
+ {
+	int rollno;
+	String name;
+	democlone(int rollno,String name)
+	 {
+		this.rollno=rollno;
+		this.name=name;
+	 }
+	public Object clone()throws CloneNotSupportedException
+	 {
+		return super.clone();
+	  }
+	public static void main(String args[])
+	 {
+		try{
+			democlone s1= new democlone(01, "Migen");
+			democlone  s2=(democlone)s1.clone();
+			democlone s3= new democlone(02, "Priya");
+		    
+			System.out.println(s1.rollno +""+ s1.name);
+			System.out.println(s2.rollno +""+ s2.name);
+			System.out.println(s3.rollno +""+ s3.name);
+		     }catch(CloneNotSupportedException d){}
+	   }
+
+ }
